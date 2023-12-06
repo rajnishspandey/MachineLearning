c9a07cf9f29a91ea851926bd755218dc283221be
rajnishspandey <pandeyrajnish86@yahoo.com>
2023-12-06 21:47:28 +0530

    01ML

---
+++ 01ML.py
@@ -0,0 +1,21 @@
+#data Wrangling
+#data
+#cleaning
+#validation
+#data structuring
+#EDA - Exploratory data Analysis
+#ETL - Extract Transforn and load (for strcuture data) , ELT - Extract Load and Transform (for unstructured or sem-structured data)
+#Feature Engineering
+#Data Normalization
+#Predective Modeling
+#Data Visualization
+
+#Steps
+#1 -  installation
+#2 - import
+
+import pandas as pd
+print(pd.__version__)
+
+data  = pd.read_csv("data.csv")
+print(data)
